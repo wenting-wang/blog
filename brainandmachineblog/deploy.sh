b412f89e90f14861ab8d2af57f560a925a0d5c2c
wwendi <wenting.wang.cd@outlook.com>
2022-02-17 22:13:38 +0100

    refresh

---
+++ brainandmachineblog/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+# Print out commands before executing them
+set -x
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+hugo -t mainroad
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin main
+
+# Back to the origin folder
+cd ..
+
+# rm -rf public
